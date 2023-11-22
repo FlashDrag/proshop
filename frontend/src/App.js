54be691d5cac9638ab1884bb10267a8da15abe0b
flashdrag <flashdrag@gmail.com>
2023-11-22 18:20:34 +0000

    Add basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="">
-      <h1>My App</h1>
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
