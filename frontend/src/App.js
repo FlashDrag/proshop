20b61d9686947fddbdf6d75c01dca6379ac3c442
flashdrag <flashdrag@gmail.com>
2023-11-25 14:26:17 +0000

    Reactify the routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
