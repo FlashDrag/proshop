aa1bc288aa650e9fc4dc26d4cf56ad69ee560806
flashdrag <flashdrag@gmail.com>
2023-11-26 13:06:50 +0000

    Add custom user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set custom user model as default
+AUTH_USER_MODEL = "users.User"
