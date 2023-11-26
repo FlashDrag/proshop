38bfc03ef57b14d749966584e8216cf4badaa75d
flashdrag <flashdrag@gmail.com>
2023-11-26 14:33:48 +0000

    Create Order, OrderItem and ShippingAddress models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
-from .models import Product, Review
+from .models import Product, Review, Order, OrderItem, ShippingAddress
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'count_in_stock')
+    list_display = ("name", "price", "count_in_stock")
+
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = OrderItemAdminInline,
 
 
 admin.site.register(Review)
+admin.site.register(ShippingAddress)
