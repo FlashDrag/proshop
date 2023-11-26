d95f700ff6e346f9424738545b3d657ec76d1824
flashdrag <flashdrag@gmail.com>
2023-11-26 14:08:28 +0000

    Create Product model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Product(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL, null=True
+    )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.ImageField()
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    num_reviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    count_in_stock = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
