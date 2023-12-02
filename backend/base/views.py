126ab7ccf79ade4d8675f331390e7692bbf3720b
flashdrag <flashdrag@gmail.com>
2023-12-02 20:22:19 +0000

    Attach API schema to views

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,38 +1,54 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 
 from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view(['GET'])
+@extend_schema(
+    summary="API Routes",
+    responses={
+        200: OpenApiResponse(
+            response={'type': 'array', 'items': {'type': 'string'}},
+        )
+    },
+)
+@api_view(["GET"])
 def getRoutes(request):
+    """List of all routes"""
     routes = [
-        '/api/products/',
-        '/api/products/create/',
-
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
+        "/api/products/",
+        "/api/products/create/",
+        "/api/products/upload/",
+        "/api/products/<id>/reviews/",
+        "/api/products/top/",
+        "/api/products/<id>/",
+        "/api/products/delete/<id>/",
+        "/api/products/<update>/<id>/",
     ]
     return Response(routes)
 
 
-@api_view(['GET'])
+@extend_schema(
+    summary="Products list",
+    responses={200: OpenApiResponse(response=ProductSerializer(many=True))},
+)
+@api_view(["GET"])
 def getProducts(request):
+    """List of all products"""
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
-@api_view(['GET'])
+@extend_schema(
+    summary="Product details",
+    responses={200: OpenApiResponse(response=ProductSerializer)},
+)
+@api_view(["GET"])
 def getProduct(request, pk):
+    """Product details"""
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
