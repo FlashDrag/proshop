f00a6669238a662c5db252d7a0b800f5803520f8
flashdrag <flashdrag@gmail.com>
2023-12-07 15:43:22 +0000

    Add form select functionality to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -22,13 +22,16 @@ function CartScreen() {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log(cartItems);
 
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
+
+  const removeFromCartHandler = (id) => {
+    console.log('remove', id)
+  }
 
   return (
     <Row>
@@ -46,10 +49,37 @@ function CartScreen() {
                   <Col md={2} className="my-auto">
                     <Image src={item.image} alt={item.name} fluid rounded />
                   </Col>
+
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
+
                   <Col md={2}>${item.price}</Col>
+
+                  <Col md={4}>
+                    <Form.Select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(addToCart(item.product, e.target.value))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Col>
+
+                  <Col md={1}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
                 </Row>
               </ListGroup.Item>
             ))}
