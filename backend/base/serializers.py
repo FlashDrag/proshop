d339338236c56a83cf7614724ffe72dae078e794
flashdrag <flashdrag@gmail.com>
2023-12-07 21:31:52 +0000

    Customize token claims

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import Product
+
+
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/customizing_token_claims.html
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["email"] = self.user.email
+
+        return data
 
 
 class ProductSerializer(serializers.ModelSerializer):
