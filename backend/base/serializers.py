ddbb8593ef8ebc6b8c75a1e16f803e681969b4f3
flashdrag <flashdrag@gmail.com>
2023-12-09 20:26:28 +0000

    Customize ProductSerializer fields in camel case name convention

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -4,6 +4,21 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # create custom fields for the serializer instead
+    # of python convention field names like 'num_reviews'
+    numReviews = serializers.SerializerMethodField()
+    countInStock = serializers.SerializerMethodField()
+    createdAt = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ("num_reviews",  "count_in_stock", "created_at")
+
+    def get_numReviews(self, obj):
+        return obj.num_reviews
+
+    def get_countInStock(self, obj):
+        return obj.count_in_stock
+
+    def get_createdAt(self, obj):
+        return obj.created_at
