be770414e8e19f0a62386a2cb561a30f2b8bedb3
flashdrag <flashdrag@gmail.com>
2023-12-31 13:57:43 +0000

    Add logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -43,3 +43,10 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({
+    type: USER_LOGOUT,
+  });
+}
